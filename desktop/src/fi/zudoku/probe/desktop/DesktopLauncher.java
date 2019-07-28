48cc15cdc600d9b563ca0ce151b8e60eb0837eef
Arap <arttu.siren@gmail.com>
2019-07-28 11:36:57 +0300

    initial commit

---
+++ desktop/src/fi/zudoku/probe/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package fi.zudoku.probe.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import fi.zudoku.probe.ProbeGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ProbeGame(), config);
+	}
+}
